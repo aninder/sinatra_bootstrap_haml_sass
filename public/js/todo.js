6789a51b53bf1e3bb547e1487c24ee217c8d7528
aninder <aninder@gmail.com>
2015-10-09 19:17:53 +0530

    more todos

--- public/js/todo.js
+++ public/js/todo.js
@@ -3,38 +3,70 @@ $(function(){
         $('#home').hide();
         $('#add').show();
     });
+    $('#clearalltodo').click(function(e){
+        if(confirm('are you sure')) {
+            localStorage.removeItem('todos');
+            localStorage.removeItem('lastitem');
+        }
+    });
     $('.jumbotron .pull-left').click(function(event){
         $('#home').show();
         $('#add').hide();
+        $('#edit').hide();
     });
-    $('.jumbotron .pull-right').click(function(event){
+    $('#add .pull-right').click(function(event){
         $('#addform').submit();
     });
+
+    $('#edit .pull-right').click(function(event){
+        alert('edit cancel')
+    });
     $('#addform').submit(function(){
-        var name = $('#todoname').val();
-        var date = $('#tododate').val();
+        var name = $('#todoaddname').val();
+        var date = $('#todoadddate').val();
+        if (name == "" || date == "" ) {
+            alert('name and date cannot be blank');
+            return false ;
+        }
         var todoarr = JSON.parse(localStorage.getItem('todos')) || [] ;
+        var new_id  = (JSON.parse(localStorage.getItem('lastid')) || 0)  + 1;
         var new_obj = {
+            id:new_id,
             name:name,
             date:date
         };
         todoarr.push(new_obj);
         localStorage.setItem('todos',JSON.stringify(todoarr));
+        localStorage.setItem('lastid',JSON.stringify(new_id));
         //alert(JSON.stringify(todoarr));
         $('#home').show();
         $('#add').hide();
         jQuery.event.trigger('mycustomevent');
     });
     $(document).bind('mycustomevent', function (e) {
-        alert('cusomt event received'+ e.targetName);
         var todoarr = JSON.parse(localStorage.getItem('todos'));
         $('#listtodos').html('');
         $.each(todoarr, function(index, item){
-            $('#listtodos').append("<li class='list-group-item'>"+todoarr[index]['name']+" "+todoarr[index]['date']+"</li>");
+            $('#listtodos').append("<li class='list-group-item' id="+item['id']+" name="+item['name']+" date="+item['date']+">"+item['name']+" "+item['date']+"<span class='glyphicon glyphicon-pencil pull-right'</span</li>");
         });
     });
     $('#home').show(function(){
         jQuery.event.trigger('mycustomevent');
     });
+    $('#listtodos').delegate('li','click', (function(event){
+        $('#home').hide();
+        $('#edit').show();
+        alert($(event.target).attr('date'));
+        //$('#todoeditname').val()
+
+    }));
 });
+
+
+
+
+
+
+
+
 
